5e6fce2855f01f8ae347da30453ada0fca497c19
DominiqueYoumans <ddyoumans@icloud.com>
2020-03-06 08:46:10 -0800

    initial commit.

---
+++ WordsTally/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordsTally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
