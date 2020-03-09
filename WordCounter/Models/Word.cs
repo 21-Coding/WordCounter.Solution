13eae8e836a81abbd220ccd2d7b5daaebacd200f
DominiqueYoumans <ddyoumans@icloud.com>
2020-03-08 22:22:07 -0700

    add HowMany() method.

--- WordCounter/Models/Word.cs
+++ WordCounter/Models/Word.cs
@@ -1,14 +1,25 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Counter.Models
 {
   public class Word
   {
+    public string FullSent { get; set; }
+    public string Count { get; set; }
     public static char[] _numbersArr = new Char[]
     {'1','2','3','4','5','6','7','8','9','0'};
     public static char[] _symbolsArr = new Char[]
     {'!', '@', '#', '$', '^', '&', '*', '(', ')','.','-','=','+',',','_'};
+
+
+    public Word(string sent, string word)
+    {
+      FullSent = sent;
+      Count = word;
+    }
+
 
     public static string SymbolChecker(string word)
     {
@@ -17,5 +28,16 @@ namespace Counter.Models
 
       return chompAll;
     }
+
+    public int HowManyWordsASent()
+    {
+
+      string[] headCounts = FullSent.Split();
+      var iFound = from head in headCounts
+                  where head == Count
+                  select head;
+      return iFound.Count();
+    }
+
   }
 }
