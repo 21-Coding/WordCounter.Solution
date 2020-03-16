3d859f54595df880ef14bc6c91bef0fa9293b9a6
DominiqueYoumans <ddyoumans@icloud.com>
2020-03-15 23:50:45 -0700

    add QuickText method.

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -4,6 +4,14 @@ namespace WordsTally
 {
     class Program
     {
+      public static void QuickText(string line)
+      {
+        for (int i = 0; i < line.Length; i++)
+          {
+            Console.Write(line[i]);
+            System.Threading.Thread.Sleep(50);
+          }
+      }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
